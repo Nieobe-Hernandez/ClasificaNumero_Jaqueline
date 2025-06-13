e0eb44566c5984d8800fc52cb9416ef7e8661dc1
Nieobe Hernandez <20223tn057@utez.edu.mx>
2025-06-12 18:29:28 -0600

    [FEATURE] subida de instrucciones para prueba de commit

---
+++ ClasificaNumero.py
@@ -0,0 +1,6 @@
+##Desarrolla un programa que permita ingresar un número y mediante un menú interactivo
+##realizar las siguientes verificaciones:
+#✓ Determinar si el número es par o impar
+#✓ Determinar si es positivo, negativo o cero
+#✓ Verificar si es múltiplo de 5
+#✓ Verificar si es divisible entre 3 y 4 al mismo tiempo
